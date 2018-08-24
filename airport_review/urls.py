f45829434a2c327867a5d30a3b3728bee72bb22c
Shimpei Okamoto <muscleconsole@gmail.com>
2018-08-24 13:14:24 +0900

    [追加]index.htmlとbase.html

--- airport_review/urls.py
+++ airport_review/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from top.views import index
 urlpatterns = [
+    path('', index, name='home'),
     path('admin/', admin.site.urls),
 ]
