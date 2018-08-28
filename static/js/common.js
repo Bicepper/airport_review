2ce61696b3360321ea61bef94520c06bc6b8ba3a
Shimpei Okamoto <muscleconsole@gmail.com>
2018-08-28 16:44:54 +0900

    [修正]common.jsにfilterdDataを追加

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,12 @@
-new Vue({
+var app = new Vue({
     el: '#app',
     data: () => ({
         items:['成田（NRT）', '羽田（ABC）', 'Fizz', 'Buzz']
-    })
+    }),
+    computed: {
+        filterdData() {
+			let options = this.options.opt_city
+            return options.filter(o => o.dependency == this.support.home_province)
+        }
+    }
 })
