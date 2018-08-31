1fb6ab3b8811ac3f92939c4179c9ca6b3dc86b11
Shimpei Okamoto <muscleconsole@gmail.com>
2018-08-31 16:24:29 +0900

    [修正]一覧のデザイン修正

--- static/js/common.js
+++ static/js/common.js
@@ -4,6 +4,9 @@ var app = new Vue({
         return {
             drawer:null,
             items:['成田（NRT）', '羽田（ABC）', 'Fizz', 'Buzz'],
+            countries:['アメリカ', '日本', '中国', 'フランス'],
+            alliance:['スターアライアンス', 'ワンワールド', 'スカイチーム'],
+            airline:['全日空', 'ルフトハンザ', 'ブリティッシュエアウェイズ', '中国東方航空'],
             cards:[
                 { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3, mdflex: 6 },
                 { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3, mdflex: 6 },
