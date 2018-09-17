0b3372b7b2415b6042c45255a11bb5edae51eef7
Shimpei Okamoto <muscleconsole@gmail.com>
2018-09-17 20:28:19 +0900

    [fix]change image path to relative

--- airport/serializers.py
+++ airport/serializers.py
@@ -4,6 +4,8 @@ from .models import Airport
 
 
 class AirportSerializers(serializers.ModelSerializer):
+    main_image = serializers.SerializerMethodField()
+
     class Meta:
         model = Airport
         fields = (
@@ -15,3 +17,7 @@ class AirportSerializers(serializers.ModelSerializer):
             'main_image',
             'country_id',
         )
+
+    def get_main_image(self, obj):
+        return str('/{a}'.format(a=obj.main_image))
+
