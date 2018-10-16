e3a44612c5e29e1079c7940bc0dda4bf0ecf9fdc
Shimpei Okamoto <muscleconsole@gmail.com>
2018-10-16 11:02:35 +0900

    [fix]Change the ReviewCreateForm init.

--- review/forms.py
+++ review/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.shortcuts import get_object_or_404
 from airport.models import Airport
 from .models import Review
 
@@ -8,6 +8,14 @@ class ReviewCreateForm(forms.ModelForm):
     """
     レビュー作成フォーム
     """
+    def __init__(self, data=None, *args, **kwargs):
+        if data is not None:
+            data = data.copy()
+            if data['airport']:
+                obj = get_object_or_404(Airport, name_ja=data['airport'])
+                data['airport'] = obj.id
+        super(ReviewCreateForm, self).__init__(data=data, *args, **kwargs)
+
     class Meta:
         model = Review
         fields = (
@@ -20,7 +28,7 @@ class ReviewCreateForm(forms.ModelForm):
             'review_title',
             'review_text',
         )
-        widgets = {
-            'airport': forms.TextInput,
-            'rate_clean': forms.TextInput(attrs={'name':'rate_clean'}),
-        }
+        # widgets = {
+        #     'airport': forms.TextInput,
+        #     'rate_clean': forms.TextInput(attrs={'name':'rate_clean'}),
+        # }
