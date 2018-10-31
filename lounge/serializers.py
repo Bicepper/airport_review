04774a16e4a767a68f254b913c7ddcb334ccd820
Shimpei Okamoto <muscleconsole@gmail.com>
2018-10-31 11:59:01 +0900

    [update]ラウンジapiのairlineをオブジェクトに変換

--- lounge/serializers.py
+++ lounge/serializers.py
@@ -3,12 +3,15 @@ from rest_framework import serializers
 from .models import Lounge
 
 from airport.models import Airport
+from airline.models import Airline
 from airport.serializers import AirportSerializers
+from airline.serializers import AirlineSerializers
 
 
 class LoungeSerializers(serializers.ModelSerializer):
     main_image = serializers.SerializerMethodField()
     airport = serializers.SerializerMethodField()
+    airline = serializers.SerializerMethodField()
 
     class Meta:
         model = Lounge
@@ -23,11 +26,12 @@ class LoungeSerializers(serializers.ModelSerializer):
         )
 
     def get_airport(self, obj):
-        print('================louneのAPI=================')
-        print(obj.airport)
-        print('================louneのAPI=================')
-        airport_abstruct = AirportSerializers(Airport.objects.all().filter(airport=Airport.objects.get(id=obj.airport)))
-        return obj.airport
+        airport_abstruct = AirportSerializers(Airport.objects.get(name_ja=obj.airport)).data
+        return airport_abstruct
+
+    def get_airline(self, obj):
+        airline_abstruct = AirlineSerializers(Airline.objects.get(name_ja=obj.airline)).data
+        return airline_abstruct
 
     def get_main_image(self, obj):
         return str('/{a}'.format(a=obj.main_image))
